6e471a9aad7d138dd425363aaf421ecf1258495c
LuisBtz <luisbttf@gmail.com>
2020-08-25 22:26:11 -0500

    images

--- src/components/left.js
+++ src/components/left.js
@@ -1,5 +1,26 @@
 import React from "react"
 import styled from "styled-components"
+import { graphql, useStaticQuery } from "gatsby"
+import Image from "gatsby-image"
+
+const getImages = graphql`
+  {
+    left: file(relativePath: { eq: "bg-left.jpg" }) {
+      childImageSharp {
+        fluid {
+          src
+        }
+      }
+    }
+    right: file(relativePath: { eq: "bg-right.jpg" }) {
+      childImageSharp {
+        fluid {
+          src
+        }
+      }
+    }
+  }
+`
 
 export const LeftContainer = styled.div`
   background-color: #30e9ff;
@@ -9,8 +30,15 @@ export const LeftContainer = styled.div`
   font-size: 2rem;
 `
 
-export default () => (
-  <LeftContainer>
-    <h1>Hello from left</h1>
-  </LeftContainer>
-)
+const Left = () => {
+  const data = useStaticQuery(getImages)
+  console.log(data)
+
+  return (
+    <LeftContainer>
+      <h1>Hello from left</h1>
+    </LeftContainer>
+  )
+}
+
+export default Left
